07ab65c54c39242af768b719adf418f5426ea5f2
Simhadri3014 <kavali.14.nani@gmail.com>
2025-07-12 15:07:53 +0530

    Add files via upload

---
+++ JAVA_STRUCTURE/Day1/Problem1/bin/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"InputReader"},{"p":"<Unnamed>","l":"IntegerFormatValidator"},{"p":"<Unnamed>","l":"PrimeNumberFinder"},{"p":"<Unnamed>","l":"Validator"}];updateSearchResults();
