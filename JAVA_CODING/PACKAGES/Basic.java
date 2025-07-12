8502101300c21ec2c62cc9f810e785f8452dd6a3
Simhadri3014 <kavali.14.nani@gmail.com>
2025-07-12 14:24:57 +0530

    Add files via upload

---
+++ JAVA_CODING/PACKAGES/Basic.java
@@ -0,0 +1,10 @@
+import src.HelloWorld;
+public class Basic
+{
+  public static void main(String[] args)
+   {
+     HelloWorld b=new HelloWorld();
+     String print="Hello World";
+     b.printing(print);
+   }
+}
---
+++ JAVA_CODING/PACKAGES/src/HelloWorld.java
@@ -0,0 +1,12 @@
+package src;
+public class HelloWorld
+{
+  //public void printing(String print)
+   //{
+  //   System.out.println(print);
+  // }
+	public static void main(String[] args)
+	{
+		System.out.println("Hi good");
+	}
+}
